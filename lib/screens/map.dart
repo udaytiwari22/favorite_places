d9461784cad58a110fe62fe136c25362f024a9ee
Uday Tiwari <udaytiwaricyef2001@gmail.com>
2024-11-27 10:39:47 +0530

    montserrat_texts

--- lib/screens/map.dart
+++ lib/screens/map.dart
@@ -47,45 +47,44 @@ class _MapScreenState extends State<MapScreen> {
         ],
       ),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            Text(
-              'Enter Latitude and Longitude',
-              style: Theme.of(context).textTheme.titleLarge,
-            ),
-            const SizedBox(height: 10),
-            TextField(
-              decoration: const InputDecoration(labelText: 'Latitude'),
-              keyboardType: TextInputType.number,
-              onChanged: (value) {
-                _latitude = double.tryParse(value);
-              },
-            ),
-            const SizedBox(height: 10),
-            TextField(
-              decoration: const InputDecoration(labelText: 'Longitude'),
-              keyboardType: TextInputType.number,
-              onChanged: (value) {
-                _longitude = double.tryParse(value);
-              },
-            ),
-            const SizedBox(height: 20),
-            if (!widget.isSelecting)
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
               Text(
-                'Selected Location: (${widget.location.latitude}, ${widget.location.longitude})',
-                style:
-                    TextStyle(color: Theme.of(context).colorScheme.onSurface),
+                'Enter Latitude and Longitude',
+                style: Theme.of(context).textTheme.titleLarge,
               ),
-            if (_latitude != null && _longitude != null)
-              Text(
-                'Preview Location: ($_latitude, $_longitude)',
-                style:
-                    TextStyle(color: Theme.of(context).colorScheme.onSurface),
+              const SizedBox(height: 10),
+              TextField(
+                decoration: const InputDecoration(labelText: 'Latitude'),
+                keyboardType: TextInputType.number,
+                onChanged: (value) {
+                  _latitude = double.tryParse(value);
+                },
               ),
-          ],
-        ),
-      ),
+              const SizedBox(height: 10),
+              TextField(
+                decoration: const InputDecoration(labelText: 'Longitude'),
+                keyboardType: TextInputType.number,
+                onChanged: (value) {
+                  _longitude = double.tryParse(value);
+                },
+              ),
+              const SizedBox(height: 20),
+              if (!widget.isSelecting)
+                Text(
+                  'Selected Location: (${widget.location.latitude}, ${widget.location.longitude})',
+                  style:
+                      TextStyle(color: Theme.of(context).colorScheme.onSurface),
+                ),
+              if (_latitude != null && _longitude != null)
+                Text(
+                  'Preview Location: ($_latitude, $_longitude)',
+                  style:
+                      TextStyle(color: Theme.of(context).colorScheme.onSurface),
+                ),
+            ],
+          )),
     );
   }
 }
